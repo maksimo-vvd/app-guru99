8bc8f85628ec1a12b19074cc99766ab2fd6a9021
maksimo-vvd <maksim.o@vevidi.com>
2022-03-11 16:14:22 +0200

    Working with External CSS in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello_comp from './addstyle.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  Hello_comp,
   document.getElementById('root')
 );
 
