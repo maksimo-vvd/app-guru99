c3226c96519f95609c0f830948218076ef73f19d
maksimo-vvd <maksim.o@vevidi.com>
2022-03-11 15:25:15 +0200

    Working with events in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import EventTest from './events.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EventTest />
   </React.StrictMode>,
   document.getElementById('root')
 );
