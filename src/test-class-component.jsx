64d57468bbea2faf1eece42b3212eee105bfa675
maksimo-vvd <maksim.o@vevidi.com>
2022-03-11 13:43:49 +0200

    Main branch. After create App.

--- src/test-class-component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class Hello extends React. Component {
-  render() {
-    return <h1>Hello, from Guru99 Tutorials! Class Component.</h1>;
-  }
-}
-export default Hello;
--- src/test-func.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function Hello() {
-  return <h1>Hello, from Guru99 Tutorials! Function.</h1>;
-}
-// const Hello_comp = <Hello />;
-// export default Hello_comp;
-//or
-export default <Hello />;
