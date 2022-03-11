eb2b05c3a37919a09c1f8f45c84f730f658869b1
maksimo-vvd <maksim.o@vevidi.com>
2022-03-11 13:36:30 +0200

    JSX, function and class component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Not working
+// const display = "Hello, from Guru99 Tutorials!";
+// const h1tag = "<h1>{display}</h1>";
+
+// Working
+// import h1tag from './test.jsx';
+// or
+// const h1tag = (<h1>{display}</h1>);
+
+// import h1tag from './test.jsx';
+
+// Function - export default <Hello />;
+// import Hello from './test-func.jsx';
+
+// Class - export default Hello;
+import Hello from './test-class-component.jsx';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // <h1>Hello, from Guru99 Tutorials!</h1>,
+  // h1tag,
+  // Hello, // Function - export default <Hello />;
+  <Hello />, // Class - export default Hello;
   document.getElementById('root')
 );
 
---
+++ src/test-func.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Hello() {
+  return <h1>Hello, from Guru99 Tutorials! Function.</h1>;
+}
+// const Hello_comp = <Hello />;
+// export default Hello_comp;
+//or
+export default <Hello />;
